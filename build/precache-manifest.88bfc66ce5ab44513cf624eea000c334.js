cc2d843f4218f478c60bc6f7dc66f052e8ed1e33
deriksadiki <deriksadiki@gmail.com>
2020-05-19 11:56:09 +0200

    new

---
+++ build/precache-manifest.88bfc66ce5ab44513cf624eea000c334.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d60d6a40380250ca0c7cc0497e00b26e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f49a5ab6675adc3e5f21",
+    "url": "/static/css/main.3e4908b5.chunk.css"
+  },
+  {
+    "revision": "0200bfff18384e27610b",
+    "url": "/static/js/2.0b702f1c.chunk.js"
+  },
+  {
+    "revision": "f49a5ab6675adc3e5f21",
+    "url": "/static/js/main.0065dbbd.chunk.js"
+  },
+  {
+    "revision": "92a426d1fc693c5a5544",
+    "url": "/static/js/runtime-main.c81bda3a.js"
+  },
+  {
+    "revision": "20d29e9b619acaf6eed9171763b2785f",
+    "url": "/static/media/area_graph.20d29e9b.png"
+  },
+  {
+    "revision": "5a6b6e25e443089d6edccf6f661b2006",
+    "url": "/static/media/lastLine.5a6b6e25.png"
+  },
+  {
+    "revision": "587e6a654bf5816618df9ced0c49dd79",
+    "url": "/static/media/newLine.587e6a65.png"
+  },
+  {
+    "revision": "c8b3476830b04d801276d17bd7befb5a",
+    "url": "/static/media/pie.c8b34768.jpg"
+  },
+  {
+    "revision": "3a2db68d71ecf34703d1f2c823ba4eda",
+    "url": "/static/media/storm.3a2db68d.jpg"
+  }
+]);
