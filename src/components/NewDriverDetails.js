d703e30e0e4d22a7311f40acc69e8ce6ab68dd45
Will1ngt0n <willington.mnisi@gmail.com>
2021-09-17 05:20:40 +0200

    added email new endpoints

--- src/components/NewDriverDetails.js
+++ src/components/NewDriverDetails.js
@@ -176,7 +176,7 @@ export default class DriverDetails extends React.Component{
       if(xhr.status == '200' && xhr.readyState == '4') {
         let resp = xhr.responseText;
         console.log(resp);
-        console.log('msg gone: ', true);
+        console.log('msg gone: truerejec');
       }
     }
     xhr.send();
